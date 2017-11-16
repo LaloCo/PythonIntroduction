4c66d8a75b6bf2f882d5afc39e1406553c2cbd90
Eduardo Rosas <eduardo@abmx.onmicrosoft.com>
2017-11-16 12:24:00 -0600

    Reusing methods from parent class

--- inheritance.py
+++ inheritance.py
@@ -4,15 +4,21 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye Color - " + self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child constructor called")
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
-#billy_cyrus = Parent("Cyrus", "blue")
+billy_cyrus = Parent("Cyrus", "blue")
 miley_cyrus = Child("Cyrus", "blue", 100)
 
 #print(billy_cyrus.last_name)
-print(miley_cyrus.last_name)
-print(miley_cyrus.number_of_toys)
+#print(miley_cyrus.last_name)
+#print(miley_cyrus.number_of_toys)
+
+miley_cyrus.show_info()
